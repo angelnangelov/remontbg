ce815a4232ce99a7aec6527c06994c4dd48f6e2e
Angel Angelov <azsumangel@gmail.com>
2021-03-17 19:04:02 +0200

    Greeting Interceptor

--- src/main/java/com/angelangelov/remont_bg/config/AppWebConfiguration.java
+++ src/main/java/com/angelangelov/remont_bg/config/AppWebConfiguration.java
@@ -1,5 +1,6 @@
 package com.angelangelov.remont_bg.config;
 
+import com.angelangelov.remont_bg.interceptor.GreetingInterceptor;
 import com.angelangelov.remont_bg.interceptor.PageTitleInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AppWebConfiguration implements WebMvcConfigurer {
     private final PageTitleInterceptor pageTitleInterceptor;
+    private final GreetingInterceptor greetingInterceptor;
 
     @Autowired
-    public AppWebConfiguration(PageTitleInterceptor pageTitleInterceptor) {
+    public AppWebConfiguration(PageTitleInterceptor pageTitleInterceptor, GreetingInterceptor greetingInterceptor) {
         this.pageTitleInterceptor = pageTitleInterceptor;
+        this.greetingInterceptor = greetingInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+
         registry.addInterceptor(pageTitleInterceptor);
+        registry.addInterceptor(greetingInterceptor);
     }
 }
---
+++ src/main/java/com/angelangelov/remont_bg/interceptor/GreetingInterceptor.java
@@ -0,0 +1,39 @@
+package com.angelangelov.remont_bg.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+
+@Component
+public class GreetingInterceptor implements HandlerInterceptor{
+
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        String greetings = "";
+        LocalDateTime localDateTime = LocalDateTime.now();
+        int hour = localDateTime.getHour();
+
+        if(hour<12){
+            greetings="Добро утро, ";
+        }else  if(hour>12 && hour<18){
+            greetings="Добър ден, ";
+        }else {
+            greetings="Добър вечер, ";
+        }
+        if (modelAndView == null) {
+            modelAndView = new ModelAndView();
+        } else {
+            if (handler instanceof HandlerMethod) {
+                modelAndView
+                        .addObject("greetings", greetings);
+            }
+        }
+
+    }
+}
